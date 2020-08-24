b225511e1dffb4f0e1b0b212398f449b68b33dd8
Mateusz Choma <kontakt@coderoad.pl>
2020-08-24 09:15:53 +0200

    babel-loader added

--- webpack.config.js
+++ webpack.config.js
@@ -36,11 +36,21 @@ module.exports = {
             {
                 test: /\.(png|jpe?g|gif)$/i,
                 use: [
-                  {
-                    loader: 'file-loader',
-                  },
+                    {
+                        loader: 'file-loader',
+                    },
                 ],
-              },
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
         ],
     },
 }
